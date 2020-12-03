d6eb4a52686e8bc3354620b967528ef7af520f25
William Truppi <williamt@hotmail.it>
2020-12-03 14:54:08 +0100

    Creare un oggetto e Stampare a schermo tutte le proprietà.

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/* Istruzioni:
+ - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+ - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+ - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+ */
+
+$(function (){
+
+  var student = {
+    "nome" : "Luca",
+    "cognome" : "Nervi",
+    "età" : 35
+  }
+
+  for (var key in student) {
+    console.log(key, student[key]);
+  }
+
+})
