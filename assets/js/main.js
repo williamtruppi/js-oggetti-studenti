2e78669e586ca06b56c826d434ea454fc9800d06
William Truppi <williamt@hotmail.it>
2020-12-03 15:39:10 +0100

    attraverso 3 prompt aggiungere un nuovo oggetto con: nome, cognome e età

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /* Istruzioni:
  - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
- - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
- - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
  */
 
 $(function (){
 
   var student = {
     "nome" : "Luca",
-    "cognome" : "Nervi",
+    "Cognome" : "Nervi",
     "età" : 35
   }
 
   for (var key in student) {
     console.log(key, student[key]);
   }
+
+/* - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome */
 
   var classeBoolean =
   [
@@ -37,6 +37,29 @@ $(function (){
 
   for (var key in classeBoolean) {
     console.log(classeBoolean[key].nome, classeBoolean[key].Cognome);
+  }
+
+  /* - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+  var name = prompt("Inserisci il nome dello studente");
+  var surname = prompt("Inserisci il cognome dello studente");
+  var age = Number(prompt("Inserisci l'età dello studente"));
+
+  console.log(name, surname, age);
+
+  classeBoolean.push(
+    {
+      "nome" : name,
+      "Cognome" : surname,
+      "età" : age
+    }
+  )
+  
+  classeBoolean.push({student});
+  
+
+  for (var key in classeBoolean) {
+    console.log(classeBoolean[key]);
   }
 
 })
